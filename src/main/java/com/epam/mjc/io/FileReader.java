73e0cfba640cf2a1e3ecb888d084d4aff36f82cd
Daria <dashatur100500@gmail.com>
2023-04-20 14:21:30 +0200

    finished task

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,29 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        String[] data = new String[4];
+        try(BufferedReader br = new BufferedReader(new java.io.FileReader(file))){
+            String line = null;
+            int i = 0;
+            while((line=br.readLine())!=null) {
+                data[i] = line.split(": ")[1];
+                i++;
+            }
+        } catch (FileNotFoundException e){
+
+        } catch (IOException e){
+
+        }
+        profile.setName(data[0]);
+        profile.setAge(Integer.parseInt(data[1]));
+        profile.setEmail(data[2]);
+        profile.setPhone(Long.parseLong(data[3]));
+        return profile;
     }
 }
